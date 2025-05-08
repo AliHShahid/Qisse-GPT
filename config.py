3920b8cf8ae5ea9fa44ab5763d0c18a871f844da
unknown <alyhassanshahid@gmail.com>
2025-05-08 13:54:06 +0500

    Updated app file

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 
-QDRANT_URL = "https://6058cda6-8eed-45cd-af5c-55b0db597fc2.us-west-1-0.aws.cloud.qdrant.io:6333"
-QDRANT_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3MiOiJtIn0.2k7LrVa7tBBYgBtdPkpmZj_3YKhXyQdyWlseSO54RFY"
+QDRANT_URL = "---"
+QDRANT_API_KEY = "---"
 COLLECTION_NAME = "Poetry DB VSCode"
 
-GROQ_API_KEY = "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"
+GROQ_API_KEY = "---"
 EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
